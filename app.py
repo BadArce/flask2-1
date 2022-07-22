c96b6796d71ba8dc24e583e374912c96626b5446
Michael Arce <lee.michael.anthony.arce@gmail.com>
2022-07-22 02:07:26 -0600

    6/14 assignment, added create_all func

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 import psycopg2
+import json
+# import 
 
 app = Flask(__name__)
 
@@ -110,7 +112,7 @@ def delete_user_by_id(user_id):
 
 @app.route('/users/list', methods=(['GET']))
 def get_all_users():
-  results = cursor.execute('SELECT user_id, first_name, last_name, email, password, size, city, state, organization FROM Users')
+  results = cursor.execute('SELECT user_id, first_name, last_name, email, password, city, state, organization FROM Users')
   results = cursor.fetchall()
   list_of_users = []
 
@@ -247,9 +249,35 @@ def create_all():
         REFERENCES Organizations (org_id)
     );
   ''')
+  data =  [["1", "Octo Studios", "555.855.5585", "San Diego", "California"],["2", "Namco", "555.867.5309", "New York City", "New York"],["3", "Skynet Tech.", "555.012.3456", "Los Angeles", "California"]]
+  for x in data:
+    org_id = x[0]
+    print(org_id)
+    name = x[1]
+    phone = x[2]
+    city = x[3]
+    state = x[4]
+    check = cursor.execute((f'SELECT org_id FROM Organizations WHERE org_id = {org_id}'))
+    check = cursor.fetchone()
+    if check == None:
+      cursor.execute("INSERT INTO Organizations (org_id, name, phone, city, state) VALUES(%s,%s,%s,%s,%s)",(org_id, name, phone, city, state))
+  data = [["100","Mark","Hoppus","mark@hoppus.net", "HappyHolidays123", "San Diego", "California","1"],["101","Tom","Delonge","tom@hoppus.net","Tak30ff!","San Diego","California","1"],["102","Travis","Barker","travis@hoppus.net","h0t4th3Kards","Los Angeles","California","1"],["103","Yugi","Yami","Yugi@oh.io","Heart0ftheC4rds","Tokyo","Japan","2"]]
+  for x in data:
+    user_id = x[0]
+    first_name = x[1]
+    last_name = x[2]
+    email = x[3]
+    password = x[4]
+    city = x[5]
+    state = x[6]
+    organization = x[7]
+    check = cursor.execute((f'SELECT user_id FROM Users WHERE user_id = {user_id}'))
+    check = cursor.fetchone()
+    if check == None:
+      cursor.execute("INSERT INTO Users (user_id, first_name, last_name, email, password, city, state, organization) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)",(user_id, first_name, last_name, email, password, city, state, organization))
   conn.commit()
   return
 
-
 if __name__ == "__main__":
+  create_all()
   app.run()
